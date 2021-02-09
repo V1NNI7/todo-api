42abd9b76a2a9e7cd1ddfef21c408734b3f8894e
Vinicius Silveira <vinicezar800@hotmail.com>
2021-02-09 14:13:02 -0300

    API Pessoas

--- index.js
+++ index.js
@@ -1,37 +1,62 @@
-const express = require('express');
+const express = require('express'); // Framework
 const app = express();
 const port = 3000;
-const dados = require('./dados.json');
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser'); // interpretação do json
+const { Pessoa } = require('./models');
 
 app.use(bodyParser.json());
 
-app.get('/pessoas', (req, res) => {
-    res.send(dados.pessoas);
+// Listagem de todas as pessoas
+app.get('/pessoas', async (req, res) => {
+    const pessoas = await Pessoa.findAll();
+    res.status(200).json(pessoas);
 });
 
-app.post('/pessoas', (req, res) => {
-    //inserção no banco
-    //retornar o valor inserido
-    res.send(req.body);
-});
-
-app.get('/pessoas/:id', (req, res) => {
-    for (let i = 0; i < dados.pessoas.length; i += 1) {
-        if (dados.pessoas[i].id == req.params.id) {
-            res.send(dados.pessoas[i]);
+// Listagem de pessoa por ID
+app.get('/pessoas/:id', async (req, res) => {
+    const pessoa = await Pessoa.findAll({
+        where: {
+            id: req.params.id
         }
-    }
+    });
+    res.status(200).json(pessoa);
 });
 
-app.put('/pessoas/:id', (req, res) => {
-    //atualização no banco de dados
-    res.send(req.body);
+// Listagem de pessoa por tipo
+app.get('/pessoas/tipo/:tipo', async (req, res) => {
+    const tipoPessoa = await Pessoa.findAll({
+        where: {
+            tipo: req.params.tipo
+        }
+    });
+    res.status(200).json(tipoPessoa);
 });
 
-app.delete('/pessoas/:id', (req, res) => {
-    //remoção do banco de dados
-    res.send(req.body);
+// Cadastro de nova pessoa
+app.post('/pessoas', async (req, res) => {
+    const pessoa = await Pessoa.create(req.body);
+    res.status(201).json(pessoa);
 });
+
+// Atualização de pessoa
+app.put('/pessoas/:id', async (req, res) => {
+    const pessoa = await Pessoa.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    });
+    res.status(200).json(pessoa);
+});
+
+// Remoção de pessoa
+app.delete('/pessoas/:id', async (req, res) => {
+    const pessoa = await Pessoa.destroy({
+        where: {
+            id: req.params.id
+        }
+    });
+    res.status(200).json(pessoa);
+});
+
 
 app.listen(port);
